3c9907f3115d5acf46af8ca50a414e6f057f4534
eldmpraz <eldmpraz@inf.ufpel.edu.br>
2019-05-10 11:41:41 -0300

    Adiciona o exercicio 2 de tp4

--- tp4/18100219_tp4_e2.asm
+++ tp4/18100219_tp4_e2.asm
@@ -9,3 +9,23 @@
 # y: .space 4
 # Observe como o método de Horner é mais eficiente (faz menos operações) que
 # calcular o polinômio de forma sequencial.
+
+.data
+.word 3
+.space 4
+
+.text 
+#instrucoes para pegar a($t1) na memoria
+lui $t0, 0x1001 
+lw $t1, 0($t0)
+#metodo de horner
+addi $t7, $t7, 5
+
+mul $t2, $t7, $t1
+addi $t3, $t2, 7
+add $t7, $t7, $t3
+
+addi $t4, $t2, 15
+add $t7, $t7, $t4
+#guarda y na memoria
+sw $t7, 4($t0)
