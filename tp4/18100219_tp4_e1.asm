ae5435785586b0c21caebd71d3042faa9f6fbabf
eldmpraz <eldmpraz@inf.ufpel.edu.br>
2019-05-10 10:16:37 -0300

    Adiciona as instrucoes de tp4

---
+++ tp4/18100219_tp4_e1.asm
@@ -0,0 +1,7 @@
+# Faça um programa que calcule a seguinte equação:
+# y = 32ab - 3a + 7b - 13
+# Utilize endereços de memória para armazenar o valore de a, b e o resultado y. Cada
+# valor deve ocupar 4 bytes na memória (.word), assim como para o resultado (.space
+# 4). Utilize as três primeiras posições da memória .data para armazenar,
+# consecutivamente, a, b e y, iniciando o código com:
+
