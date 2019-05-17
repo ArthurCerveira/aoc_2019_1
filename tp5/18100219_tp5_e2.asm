c92a4fc223ca615c167a4382ba084c21f318d5ed
arthurcerveira <ldpszczepaniak@inf.ufpel.edu.br>
2019-05-17 10:27:17 -0300

    Adiciona o E2 do TP5

--- tp5/18100219_tp5_e2.asm
+++ tp5/18100219_tp5_e2.asm
@@ -5,3 +5,21 @@
 # código com:
 # .data
 # y: .space 4
+.data
+.space 4
+
+.text
+lui $t0, 0x1001 
+#carrega 333 para comparação
+ori $t1, $zero, 333
+#carrega 1 para controle
+ori $s0, $zero, 1
+#adiciona 1 ao controle e o resultado no total
+loop: add $s1, $s0, $s1
+addi $s0, $s0, 1
+#testa se chegou no 333, senao volta ao inicio
+beq $s0, $t1, fim
+j loop
+#guarda o resultado na memória
+fim: sw $s1, 0($t0)
+
