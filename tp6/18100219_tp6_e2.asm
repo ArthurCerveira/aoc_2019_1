9d3288c860af5f2e6fe1ac07e2ea286b9d462ff5
Renã Souza <rfsouza@inf.ufpel.edu.br>
2019-05-24 10:51:24 -0300

    Adiciona os enunciados do trabalho 6

---
+++ tp6/18100219_tp6_e2.asm
@@ -0,0 +1,5 @@
+#Escreva um programa que calcule o produtório abaixo. O valor de n deve ser lido da
+#posição 0x10010000 da memória no início do programa. O valor de A deve ser
+#escrito na memória no fim do programa, na posição 0x10010004.
+#i=0 até n, produtorio: n+(i/2)
+#OBS: considere a divisão inteira
