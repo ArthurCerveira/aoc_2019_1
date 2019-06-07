27a42b3f0ebfa88e23897ca7ef98273c8c5f1d78
arthurcerveira <eldmpraz@inf.ufpel.edu.br>
2019-06-07 10:29:29 -0300

    Adiciona instrucoes do trabalho 7

---
+++ tp7/18100219_tp7_e2.asm
@@ -0,0 +1,10 @@
+#Escreva um programa que altere uma string para “capitalizar” a primeira letra de
+#cada palavra. Por exemplo, a entrada
+#.data
+#string: .asciiz "meu professor é o melhor"
+#deve produzir a string
+#"Meu Professor É O Melhor"
+#Assuma que a entrada possui apenas espaços e letras minúsculas. Pode haver mais
+#de um espaço entre as palavras.
+#A resposta deve ser a string de entrada modificada, e não uma nova string na
+#memória, ou seja, iniciando no endereço de memória 0x10010000.
