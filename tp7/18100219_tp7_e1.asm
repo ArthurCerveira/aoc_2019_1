b0032c1058a781aff7b9e0c0f765c5b375d58618
arthurcerveira <eldmpraz@inf.ufpel.edu.br>
2019-06-07 11:37:51 -0300

    IMplementa exercicio 1 do TP6

--- tp7/18100219_tp7_e1.asm
+++ tp7/18100219_tp7_e1.asm
@@ -8,3 +8,32 @@
 # código do espaço e do \0 (null)).
 # A resposta deve ser a string de entrada modificada, e não uma nova string na
 # memória, ou seja, iniciando no endereço de memória 0x10010000.
+
+.data
+string: .asciiz "Eu amo muito meu professor de AOC-I."
+
+.text
+#carrega space em $s0
+ori $s0, $zero, 32
+#carrega o endereco da string
+la $t0, string
+#carrega o char
+loop: lbu $t1, 0($t0)
+#se for igual a espaco pula
+beq $t1, $s0 igualespaco
+#vai para a proxima char
+proxchar: addi $t0, $t0, 1
+#testa se a string acabou
+beq $t1,$zero, fim
+#senao, volta o loop
+j loop
+
+
+#se for igual a espaco guarda null na posicao da memoria
+igualespaco: sb $zero, 0($t0)
+#volta para o loop
+j proxchar
+
+
+fim:nop
+
